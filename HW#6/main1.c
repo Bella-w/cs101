9320bf65efc8d646dcae263503e5a541ec10c782
Bella-w <51277857+Bella-w@users.noreply.github.com>
2022-12-15 16:48:37 +0800

    main.c

---
+++ HW#6/main1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    if(i%15==0)
+        printf("love IU");
+    else if(i%3==0)
+        printf("love");
+    else if(i%5==0)
+        printf("IU");
+    else
+        printf("%d\n",i);
+    return 0;
+}
