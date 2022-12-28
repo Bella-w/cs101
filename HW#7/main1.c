19fc037b01dc0f087ff35ad7d217cab9153cebb7
Bella-w <51277857+Bella-w@users.noreply.github.com>
2022-12-28 15:49:45 +0800

    Update main1.c

--- HW#7/main1.c
+++ HW#7/main1.c
@@ -3,10 +3,8 @@ int main()
 {
     int n=7;
     for(int i=1;i<=n;i++){
-        for(int j=0;j<=n-i;j++){
-            printf(" ");   
-        }
-        for(int k=1;k<=i;k++){
+        printf("%*d ",n+1-i,i);
+        for(int j=0;j<i-1;j++){
             printf("%d ",i);
         }
         printf("\n");
